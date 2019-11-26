c27ef7f058a1b5014e6cedb8e46c56f1cd109ce6
Wojciech Witon <wojtekwiton@gmail.com>
2019-11-26 17:29:04 +0000

    working version

--- start.js
+++ start.js
@@ -80,11 +80,12 @@ main = function () {
 		mqttClient.publish(topic, value, {retain: true});
 
 		var addressParts = address.split('.');
-		if (addressParts[1] === 'output') {
+		// 'int' for LED stripes
+		if (addressParts[1] === 'output' || addressParts[1] === 'int') {
 			mqttClient.publish(topic + '/_brightness_state', value, {retain: true});
 			mqttClient.publish(topic + '/_state', value === '0' ? 'OFF' : 'ON', {retain: true});
 		}
-		if (addressParts[1] === 'relay' || addressParts[1] === 'variable') {
+		if (addressParts[1] === 'relay' || addressParts[1] === 'variable' || addressParts[1] === 'input') {
 			mqttClient.publish(topic + '/_state', value === '0' ? 'OFF' : 'ON', {retain: true});
 		}
 		if (addressParts[1] === 'key' && value === 'hit') {
@@ -92,19 +93,59 @@ main = function () {
 				mqttClient.publish(topic, 'break', {retain: true});
 			}, 150);
 		}
-		if (addressParts[1] === 'regulator') {
-			var mode = 'auto';
-			if (value === '1050') {
-				mode = 'off';
+		var lastElement = addressParts[addressParts.length - 1];
+		if (addressParts[1] === 'regulator' || addressParts[1] === 'int') {
+			if (lastElement === 'mode') {
+				var mode = '';
+				if (value === '0') {
+					mode = 'auto';
+				} else if (value === '1') {
+					mode = 'heat';
+				} else if (value === '2') {
+					mode = 'dry';
+				} else if (value === '3') {
+					mode = 'fan_only';
+				} else if (value === '4') {
+					mode = 'cool';
+				}
+				if (mode !== '') {
+					mqttClient.publish(topic + '/_mode', mode, {retain: true});
+				}
+			} else if (lastElement === 'fan') {
+				var mode = '';
+				if (value === '0') {
+					mode = 'auto';
+				} else if (value === '1') {
+					mode = 'low';
+				} else if (value === '2') {
+					mode = 'medium';
+				} else if (value === '3') {
+					mode = 'high';
+				}
+				if (mode !== '') {
+					mqttClient.publish(topic + '/_fan', mode, {retain: true});
+				}
+			} else if (lastElement === 'onoff') {
+				var mode = '';
+				if (value === '0') {
+					mode = 'OFF';
+				} else if (value === '1') {
+					var lastSlashIndex = address.lastIndexOf(".");
+					var newAddress = address.substring(0, lastSlashIndex) + ".mode";
+					domiqClient.get(newAddress);
+				}
+				if (mode !== '') {
+					var lastSlashIndex = topic.lastIndexOf("/");
+					var newTopic = topic.substring(0, lastSlashIndex) + "/mode/_mode";
+					mqttClient.publish(newTopic, mode, {retain: true});
+					mqttClient.publish(topic + '/_state', mode, {retain: true});
+				}
 			}
-			if (value === '1300') {
-				mode = 'heat';
-			}
-			mqttClient.publish(topic + '/_mode', mode, {retain: true});
 		}
 		var nValue = Number(value);
-		if (addressParts[1] === 'regulator' || (addressParts[1] === 'variable' && nValue > 800 && nValue < 6000)) {
-			mqttClient.publish(topic + '/_c', ((Number(value) - 1000) / 10).toString(), {retain: true});
+		// 'int' for tempset in AC
+		if (addressParts[1] === 'regulator' || (addressParts[1] === 'variable' || addressParts[1] === 'int' && nValue > 800 && nValue < 6000)) {
+			mqttClient.publish(topic + '/_c', ((nValue - 1000) / 10).toString(), {retain: true});
 		}
 	});
 
@@ -144,38 +185,117 @@ main = function () {
 					break;
 
 				case '_set':
-				case '_brightness_set':
-					if (message.toString() === 'ON') {
+					if (message.toString() === 'ON' || message.toString() === 'on') {
 						value = 'on';
 					}
-					if (message.toString() === 'OFF') {
+					else if (message.toString() === 'OFF' || message.toString() === 'off') {
 						value = 'off';
-					}
-					if (addressParts[1] === 'output') {
-						value = value + ';ramp:4';
 					}
 					logger.info('> domiq', ' ', address, ' = ', value);
 					domiqClient.write(address, value);
 
 					break;
 
+				case '_binary_set':
+					if (message.toString() === 'ON' || message.toString() === 'on') {
+						value = '1';
+					}
+					else if (message.toString() === 'OFF' || message.toString() === 'off') {
+						value = '0';
+					}
+					logger.info('> domiq', ' ', address, ' = ', value);
+					domiqClient.write(address, value);
+
+					break;
+
+				case '_brightness_set':
+					if (message.toString() === 'OFF' || message.toString() === 'off') {
+						value = 'off';
+					}
+					if (addressParts[1] === 'output') {
+						value = value + ';ramp:6';
+					}
+					logger.info('> domiq', ' ', address, ' = ', value);
+					domiqClient.write(address, value);
+
+					break;
+
+				case '_led_brightness_set':
+					if (message.toString() === 'OFF' || message.toString() === 'off') {
+						value = '0';
+					}
+					var lastSlashIndex = address.lastIndexOf(".");
+					var oldVar = address.substring(lastSlashIndex + 1);
+					var rawAddress = address.substring(0, lastSlashIndex);
+
+					var valVar = 'dj' + oldVar;
+					var valAddress = rawAddress + '.' + valVar;
+					logger.info('> domiq', ' ', valAddress, ' = ', value);
+					domiqClient.write(valAddress, value);
+
+					var delayValue = '200';
+					var delayVar = 'cz' + oldVar;
+					var delayAddress = rawAddress + '.' + delayVar;
+					logger.info('> domiq', ' ', delayAddress, ' = ', delayValue);
+					domiqClient.write(delayAddress, delayValue);
+
+					break;
+
 				case '_mode_set':
 					value = message.toString();
-					if (message.toString() === 'on' || message.toString() === 'heat') {
-						value = '30';
+					var numValue = '';
+					var onValue = '0';
+					// heat_cool only for homekit climate control
+					if (value === 'auto' || value === 'heat_cool') {
+						numValue = '0';
+						onValue = '1';
+					} else if (value === 'heat') {
+						numValue = '1';
+						onValue = '1';
+					} else if (value === 'dry') {
+						numValue = '2';
+						onValue = '1';
+					} else if (value === 'fan_only') {
+						numValue = '3';
+						onValue = '1';
+					} else if (value === 'cool') {
+						numValue = '4';
+						onValue = '1';
 					}
-					if (message.toString() === 'off' || message.toString() === 'cool') {
-						value = '5';
+					
+					var lastSlashIndex = address.lastIndexOf(".");
+					var newAddress = address.substring(0, lastSlashIndex) + ".onoff";
+					logger.info('> domiq', ' ', newAddress, ' = ', onValue);
+					domiqClient.write(newAddress, onValue);
+
+					if (numValue !== '') {
+						logger.info('> domiq', ' ', address, ' = ', numValue);
+						domiqClient.write(address, numValue);
 					}
-					if (message.toString() === 'auto') {
-						value = '22';
+					
+					break;
+
+				case '_fan_set':
+					value = message.toString();
+					if (value === 'auto') {
+						value = '0';
 					}
-					value = (Number(value) * 10 + 1000).toString();
+					if (value === 'low') {
+						value = '1';
+					}
+					else if (value === 'medium') {
+						value = '2';
+					}
+					else if (value === 'high') {
+						value = '3';
+					}
+					logger.info('> domiq', ' ', address, ' = ', value);
 					domiqClient.write(address, value);
 					break;
 
 				case '_temp_set':
 					value = (Number(value) * 10 + 1000).toString();
+					logger.info('> domiq', ' ', address, ' = ', value);
 					domiqClient.write(address, value);
 					break;
 
